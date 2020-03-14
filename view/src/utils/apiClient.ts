7698c2b2c540ada65d205e3bc2104ffa80cd992e
Arunesh <hello@arunesh.me>
2020-03-14 18:15:15 +0100

    Cleanup

--- view/src/utils/apiClient.ts
+++ view/src/utils/apiClient.ts
@@ -1,9 +1,3 @@
-// import got, { ExtendOptions } from 'got'
-
-// const apiClient = got.extend({
-//   prefixUrl: process.env.API_URL
-// })
-
 export const swrFetcherJSON = async (path: string) => {
 	const res = await fetch(`${process.env.API_URL}${path}`)
 
