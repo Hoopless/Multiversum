49ff25ce06f2eaf64eb9f18c63acedca6e75f03c
Jasper Stolwijk <jasperstolwijk@icloud.com>
2020-03-25 12:35:11 +0100

    Tegel gefixed op de cms index.

--- view/src/pages/cms/index.tsx
+++ view/src/pages/cms/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import useSWR from 'swr'
 import CMSHeader from "../../components/cms/header"
 import { Flex, Box, Text, Button } from '@chakra-ui/core'
-import { FaPlus, FaListUl, FaEdit } from 'react-icons/fa'
+import { FaPlus, FaListUl, FaEdit, FaChartBar } from 'react-icons/fa'
 import PreloadFetch from '../../components/Utils/PreloadFetch'
 import styled from '@emotion/styled'
 import FlexBox from '../../components/shared/FlexBox'
@@ -34,7 +34,7 @@ const CMSOverview: FC = () => {
         <Flex wrap="wrap">
 
         <Link href={'/cms/product/create'}>
-            <Flex mx="5px" w="25%" h="150px" bg="#bebebe" p="10px" justifyContent="center" align="center" wrap="wrap">
+            <Flex mx="5px" w="20%" h="150px" bg="#bebebe" p="10px" justifyContent="center" align="center" wrap="wrap">
                 <Flex w='100%' justifyContent="center">
                     <FaPlus size="4rem" />
                 </Flex>
@@ -45,7 +45,7 @@ const CMSOverview: FC = () => {
         </Link>
 
         <Link href={'/cms/product/overview'}>
-            <Flex mx="5px" w="25%" h="150px" bg="#bebebe" p="10px" justifyContent="center" align="center" wrap="wrap">
+            <Flex mx="5px" w="20%" h="150px" bg="#bebebe" p="10px" justifyContent="center" align="center" wrap="wrap">
                 <Flex w='100%' justifyContent="center">
                     <FaListUl size="4rem" />
                 </Flex>
@@ -58,12 +58,23 @@ const CMSOverview: FC = () => {
 
 
         <Link href={'/cms/pages/update'}>
-            <Flex mx="5px" w="25%" h="150px" bg="#bebebe" p="10px" justifyContent="center" align="center" wrap="wrap">
+            <Flex mx="5px" w="20%" h="150px" bg="#bebebe" p="10px" justifyContent="center" align="center" wrap="wrap">
                 <Flex w='100%' justifyContent="center">
                     <FaEdit size="4rem" />
                 </Flex>
                 <Box w="100%" textAlign="center">
                     Pagina's bewerken
+                </Box>
+            </Flex>
+        </Link>
+
+        <Link href={'/cms/stats'}>
+            <Flex mx="5px" w="20%" h="150px" bg="#bebebe" p="10px" justifyContent="center" align="center" wrap="wrap">
+                <Flex w='100%' justifyContent="center">
+                    <FaChartBar size="4rem" />
+                </Flex>
+                <Box w="100%" textAlign="center">
+                    Order statistieken
                 </Box>
             </Flex>
         </Link>
